d99f02ca21d683c079444c7593f792d8e6a82d2e
sankargalla <gallasankarsai1107@gmail.com>
2025-03-26 15:40:31 -0500

    Backend Files

---
+++ giftlink-backend/routes/searchRoutes.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+const connectToDatabase = require('../models/db');
+
+// Search for gifts
+router.get('/', async (req, res, next) => {
+    try {
+
+        // Connect to MongoDB
+        const db = await connectToDatabase();
+        const collection = db.collection("gifts");
+        // Initialize the query object
+        let query = {};
+
+        // check if the name exists and is not empty
+        if (req.query.name && req.query.name.trim() !== '') {
+            query.name = { $regex: req.query.name, $options: "i" }; // Using regex for partial match, case-insensitive
+        }
+
+        // Add other filters to the query
+        if (req.query.category) {
+            query.category = req.query.category;
+        }
+        if (req.query.condition) {
+            query.condition = req.query.condition;
+        }
+        if (req.query.age_years) {
+            query.age_years = { $lte: parseInt(req.query.age_years) };
+        }
+
+        // Fetch filtered gifts
+        const gifts = await collection.find(query).toArray();
+        res.json(gifts);
+    } catch (e) {
+        next(e);
+    }
+});
+
+module.exports = router;
